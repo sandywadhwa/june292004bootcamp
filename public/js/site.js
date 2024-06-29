d564c0f409ceb35e8e3e0e2f541c49a702bf1831
Sandeep <sandeep@beingzero.in>
2024-06-29 12:16:52 +0530

    Day wise content and styles added

--- public/js/site.js
+++ public/js/site.js
@@ -1,3 +1,13 @@
 function welcome(){
-    alert("Welcome to React Bootcamp");
+    console.log("Welcome to React Bootcamp");
+}
+
+function toggleDayContent(dayNumber){
+    const dayContent = document.getElementById("day-content-"+dayNumber);
+    if(dayContent.style.display === "none"){
+        dayContent.style.display = "block";
+    }
+    else{
+        dayContent.style.display = "none";
+    }
 }
